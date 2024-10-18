cb26ca68801f41a42a856a4fb81f91c49f8b258f
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2024-10-18 12:26:29 -0300

    18/10/24

---
+++ assets/pergunta.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Futebol',
+    'Qual jogador(a) com mais gols em Copa do Mundo?',
+    'Marta.'
+)
+criaCartao(
+    'História',
+    'Qual foi a maior operação militar do mundo?',
+    'Operação Overlord, conhecido como dia D.'
+)
+criaCartao(
+    'Física/História',
+    'Qual o primeiro humano a pisar na lua?',
+    'Neil Armstrong'
+)
+criaCartao(
+    'História/Militar',
+    'Quem criou a primeira bomba âtomica do mundo?',
+    'Robert Oppenheimer'
+)
