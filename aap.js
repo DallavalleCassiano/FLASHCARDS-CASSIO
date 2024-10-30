60fcb086ceb3c8ef41de551701f45bdcbf655f08
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2024-10-30 10:17:46 -0300

    30/10

--- aap.js
+++ aap.js
@@ -16,16 +16,13 @@ function criaCartao(categoria, pergunta, resposta) {
                 </div>
 `
 
-let respostaEstaVisivel=false
-function viraCartao(){
-    respostaEstaVisivel= !respostaEstaVisivel;
-    cartao.classList.toggle('active', respostaEstaVisivel)
-}
+    let respostaEstaVisivel = false;
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
 
-cartao.addEventListener('click, viraCartao');
+    cartao.addEventListener('click, viraCartao');
 
-container.appendChild (cartao)
-
-
-container.appendChild (cartao);
+    container.appendChild(cartao);
 }
